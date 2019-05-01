a16dd456413751bb8bc4a48831b3d9764c19761d
Jacques Le Normand <jacqueslenormand@gmail.com>
2019-05-01 18:18:43 -0400

    Removed dir

--- 3800-redux-intro-search/src/data.js
+++ 3800-redux-intro-search/src/data.js
@@ -1,15 +1,15 @@
 let items = [{ // 1
-    id: "ab23s", // 1
+    id: "ab21233s", // 1
     price: 100, // 1
     name: "hat", // 1
     description: "nice hat" // 1
 }, { // 1
-    id: "ab23s", // 1
+    id: "a51b23s", // 1
     price: 200, // 1
     name: "gucci hat", // 1
     description: "nicest hat" // 1
 }, { // 1
-    id: "ab23s", // 1
+    id: "a623b23s", // 1
     price: 20000, // 1
     name: "boat", // 1
     description: "nice boat" // 1
